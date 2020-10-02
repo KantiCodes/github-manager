c3474050a205b8b8679ac4c9c66334295261db30
eviluser <jendrekpb@gmail.com>
2020-10-02 23:27:25 +0200

    added basics test and project structure

--- app/main.py
+++ app/main.py
@@ -4,9 +4,9 @@ import github_api
 if __name__ == '__main__':
     sg.theme('DarkAmber')  # Add a touch of color
     # All the stuff inside your window.
-    layout = [[sg.Text('This is a simple tool add a person with specific username to Steinwurfs Team')],
+    layout = [[sg.Text('This is a simple tool to add a person with specific username to specific team')],
               [sg.Text('Enter github user'), sg.InputText(default_text='github_api username'), sg.Button('save user')],
-              [sg.Text('Enter Steinwurfs team'), sg.InputText(default_text='research'), sg.Button('save team')],
+              [sg.Text('Enter the team name'), sg.InputText(default_text='github_team'), sg.Button('save team')],
               [sg.Button('Ok'), sg.Button('cancel')]]
 
     # Create the Window
